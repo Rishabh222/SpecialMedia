9e6d21887c419af8b0379c75ddda7d1760edd8cc
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2016-04-27 15:17:27 +0530

    added top read and unread mails functionality ,optimized writing to file

--- specialGmail/views/gmail_functions.py
+++ specialGmail/views/gmail_functions.py
@@ -81,7 +81,7 @@ def ListMessagesMatchingQuery(service, user_id, query=''):
 def ListMessagesWithLabels(service, user_id, label_ids=[]):
     try:
         response = service.users().messages().list(userId=user_id,
-                                                   labelIds=label_ids).execute()
+                                                       labelIds=label_ids).execute()
         messages = []
         if 'messages' in response:
             messages.extend(response['messages'])
@@ -140,72 +140,76 @@ def unreadMessages(service, user_id):
     for list_unread_messages_id in list_unread_messages_ids:
         messages[list_unread_messages_id['id']] = GetMessage(service, user_id, list_unread_messages_id['id'])
 
-    for msg in messages:
-        # print(msg)
-        # print(messages[msg])
-        # print ("writing", messages[msg]['snippet'])
-        try:
-            # print("writing",messages[msg]['snippet'],"from:",messages[msg]['payload']['headers'][3]['value'])
-            s = messages[msg]['snippet'].encode('ascii', 'ignore').decode('ascii')
-            frm = ""
+    writeGivenMessageIds(messages)
 
-            if messages[msg]['payload']['headers'][17][u'name'] == 'From':
-                frm = messages[msg]['payload']['headers'][17][u'value']
-                f.write("mail from " + frm + " is " + s + '\n')
-            else:
-                for headers in messages[msg]['payload']['headers']:
-                    if headers['name'] == 'From':
-                        frm = headers[u'value']
-                        f.write("mail from " + frm + " is " + s + '\n')
-                        break
+def readTopUnreadMails(service, maxResults):
+    response = service.users().messages().list(userId='me',
+                                                   q='label:unread',maxResults=maxResults).execute()
+    list_top_unread_messages_ids = []
+    if 'messages' in response:
+        list_top_unread_messages_ids.extend(response['messages'])
+    messages = {}
+    for list_messages_id in list_top_unread_messages_ids:
+        messages[list_messages_id['id']] = GetMessage(service, 'me', list_messages_id['id'])
 
-            if frm == "":
-                f.write("mail from " + messages[msg]['payload']['headers'][3]['value'][1:-1] + " is " + s + '\n')
+    writeGivenMessageIds(messages)
+def readTopMails(service, maxResults):
+    response = service.users().messages().list(userId='me', maxResults=maxResults).execute()
+    list_top_read_messages_ids = []
+    if 'messages' in response:
+        list_top_read_messages_ids.extend(response['messages'])
+    messages = {}
 
-        except Exception as e:
-            print(e)
+    for list_messages_id in list_top_read_messages_ids:
+        messages[list_messages_id['id']] = GetMessage(service, 'me', list_messages_id['id'])
 
+    writeGivenMessageIds(messages)
 
 def GetSubjects(service, user_id, query):
-    # print query
-    list_messages_ids = ListMessagesMatchingQuery(service, user_id, 'label:unread '+query)
+    list_messages_ids = ListMessagesMatchingQuery(service, user_id, 'label:unread ' + query)
 
-    f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
     messages = {}
+    all_subjects = ""
     subj_id_mapping = {}
     for list_messages_id in list_messages_ids:
         messages[list_messages_id['id']] = GetMessage(service, user_id, list_messages_id['id'])
         subject = ""
+
         # print messages[list_messages_id['id']]['payload']['headers'][16]
         if messages[list_messages_id['id']]['payload']['headers'][16][u'name'] == 'Subject':
             subject = messages[list_messages_id['id']]['payload']['headers'][16][u'value']
+
             if len(subject) == 0:
-                f.write(
-                    "No Subject " + messages[list_messages_id['id']]['snippet'].encode('ascii', 'ignore').decode(
-                        'ascii') + '\n')
+                s = messages[list_messages_id['id']]['snippet'].encode('ascii', 'ignore').decode(
+                    'ascii')
+                all_subjects += "No Subject " + messages[list_messages_id['id']]['snippet'].encode('ascii',
+                                                                                                   'ignore').decode(
+                    'ascii') + '\n'
+
             else:
-                f.write(subject + '\n')
-                # print (subject,"1")
+                all_subjects += "" + subject + '\n'
+
         else:
             for headers in messages[list_messages_id['id']]['payload']['headers']:
 
                 if headers[u'name'] == 'Subject':
                     subject = headers[u'value']
                     if len(subject) == 0:
-                        f.write(
-                            "No Subject ... content is " + messages[list_messages_id['id']]['snippet'].encode('ascii',
-                                                                                                              'ignore').decode(
-                                'ascii') + '\n')
+                        all_subjects += "No Subject ... content is " + messages[list_messages_id['id']][
+                            'snippet'].encode('ascii',
+                                              'ignore').decode(
+                            'ascii') + '\n'
                     else:
-                        f.write(" " + subject + '\n')
-                    # print subject,"2"
+                        all_subjects += " " + subject + '\n'
                     break
-        # print subject,"3"
         if len(subject) == 0:
             subj_id_mapping[list_messages_id['id']] = "No Subject"
         else:
             subj_id_mapping[list_messages_id['id']] = subject
 
+    print all_subjects
+    f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
+    f.write(all_subjects)
     for x in subj_id_mapping:
         print subj_id_mapping[x]
     return subj_id_mapping
@@ -303,3 +307,34 @@ def DeleteMessage(service, user_id, msg_id):
         print 'Message with id: %s deleted successfully.' % msg_id
     except errors.HttpError, error:
         print 'An error occurred: %s' % error
+
+def writeGivenMessageIds(messages={}):
+    write_msg=""
+    for msg in messages:
+        try:
+            # print("writing",messages[msg]['snippet'],"from:",messages[msg]['payload']['headers'][3]['value'])
+            s = messages[msg]['snippet'].encode('ascii', 'ignore').decode('ascii')
+            frm = ""
+
+            if messages[msg]['payload']['headers'][17][u'name'] == 'From':
+                frm = messages[msg]['payload']['headers'][17][u'value']
+                write_msg += "mail from " + frm + " is " + s + '\n'
+                # f.write("mail from " + frm + " is " + s + '\n')
+            else:
+                for headers in messages[msg]['payload']['headers']:
+                    if headers['name'] == 'From':
+                        frm = headers[u'value']
+                        write_msg += "mail from " + frm + " is " + s + '\n'
+                        # f.write("mail from " + frm + " is " + s + '\n')
+                        break
+
+            if frm == "":
+                write_msg += "mail from " + messages[msg]['payload']['headers'][3]['value'][1:-1] + " is " + s + '\n'
+                # f.write("mail from " + messages[msg]['payload']['headers'][3]['value'][1:-1] + " is " + s + '\n')
+
+        except Exception as e:
+            print(e)
+
+    f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
+    f.write(write_msg)
+    f.close()
--- specialmedia/management/commands/get_Instructions.py
+++ specialmedia/management/commands/get_Instructions.py
@@ -7,7 +7,7 @@ import os
 import re
 from specialGmail.views.gmail_functions import authenticateUser, unreadMessages, CreateMessage, SendMessage, \
     GetSubjects, \
-    DeleteMessage
+    DeleteMessage, ListMessagesWithLabels, readTopMails, readTopUnreadMails
 import difflib
 
 read_settimgs = settings.READ_INSTRUCTION_SETTINGS
@@ -30,9 +30,9 @@ class Command(BaseCommand):
         pass
 
 
-def start_reading(sc, last_read_time=0, subj_id_mapping={}):
+def start_reading(sc, last_read_time=0, subj_id_mapping={} , to_mail_ids=[]):
     try:
-        print subj_id_mapping,1
+        # print subj_id_mapping,1
         f = open(settings.SPEECH_TO_TEXT_FILE_NAME, 'r')
         statbuf = os.stat(settings.SPEECH_TO_TEXT_FILE_NAME)
         if statbuf.st_mtime > last_read_time:
@@ -42,25 +42,49 @@ def start_reading(sc, last_read_time=0, subj_id_mapping={}):
             print x
             if "mails" in x or "mail" in x:
                 service = authenticateUser()
-                if "unread" in x:
+
+                if "read" in x and "top" in x and "unread" not in x:
+                    num=5
+                    for y in x:
+                        if y.isdigit():
+                            num = y
+                    readTopMails(service,num)
+
+                elif "unread" in x and "top" not in x:
                     unreadMessages(service, 'me')
+
+                elif "unread" in x and "top" in x:
+                    num=5
+                    for y in x:
+                        if y.isdigit():
+                            num = y
+                    readTopUnreadMails(service,num)
+
                 elif "send" in x:
                     to_mail_ids = re.findall(r'[\w\.-]+@[\w\.-]+', lines[0])
                     f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
                     f.write("what message text u want to send ?")
-                    time.sleep(10)
+                    f.close()
+
+                elif "body" in x and "message" in x:
                     f = open(settings.SPEECH_TO_TEXT_FILE_NAME, 'r')
+                    print to_mail_ids
                     lines = f.readlines()
+                    lines[0] = " ".join(lines[0].split('+'))
+
+                    print lines
                     for x in to_mail_ids:
                         message = CreateMessage('me', x, "", " ".join(lines))
                         SendMessage(service, 'me', message)
                     print "mail sent"
                     f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
                     f.write("Mail sent to " + " ".join(to_mail_ids))
+
                 elif "delete" in x and "subject" not in x:
                     del_mail_ids = re.findall(r'[\w\.-]+@[\w\.-]+', lines[0])
                     f = open(settings.TEXT_TO_SPEECH_FILE_NAME, 'w')
                     f.write("which message u want to delete ? Here are few options :")
+                    f.close()
                     del_ids = ""
                     for ids in del_mail_ids:
                         del_ids += ids + " "
@@ -74,6 +98,7 @@ def start_reading(sc, last_read_time=0, subj_id_mapping={}):
                     #     statbuf = os.stat(settings.SPEECH_TO_TEXT_FILE_NAME)
                     # print "hello"
                     # last_read_time = statbuf.st_mtime
+
                 elif "delete" in x and "subject" in x:
                     print "hello"
                     f = open(settings.SPEECH_TO_TEXT_FILE_NAME, 'r')
@@ -83,19 +108,23 @@ def start_reading(sc, last_read_time=0, subj_id_mapping={}):
                     del_id = ''
                     print subj_id_mapping
                     for x in subj_id_mapping:
-                        if difflib.SequenceMatcher(None, x.split(' '), lines[0].split('+')) > max:
-                            max = difflib.SequenceMatcher(None, subj_id_mapping[x].split(' '), lines[0].split('+'))
+                        print x,"outer loop" , difflib.SequenceMatcher(None, subj_id_mapping[x].split(' '), lines[0].split('+')).ratio()
+                        if difflib.SequenceMatcher(None, subj_id_mapping[x].split(' '), lines[0].split('+')).ratio() > max:
+                            print del_id , max , x
+                            max = difflib.SequenceMatcher(None, subj_id_mapping[x].split(' '), lines[0].split('+')).ratio()
                             del_id = x
 
                     if del_id != '':
                         DeleteMessage(service, 'me', del_id)
 
+
+
             f.close()
 
-        sc.enter(read_settimgs.get('BATCH_TIME_INTERVAL'), 1, start_reading, (sc, last_read_time, subj_id_mapping,))
+        sc.enter(read_settimgs.get('BATCH_TIME_INTERVAL'), 1, start_reading, (sc, last_read_time, subj_id_mapping,to_mail_ids))
     except Exception as e:
         on_read_run_failed(e)
-        sc.enter(read_settimgs.get('BATCH_TIME_INTERVAL'), 1, start_reading, (sc, last_read_time, subj_id_mapping,))
+        sc.enter(read_settimgs.get('BATCH_TIME_INTERVAL'), 1, start_reading, (sc, last_read_time, subj_id_mapping,to_mail_ids))
 
 
 def on_read_run_failed(message):
